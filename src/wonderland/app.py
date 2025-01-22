7bc2f3aa7966fa482c2ee6146b72e628c4306ed0
Jacob Bridges <me@jacobbridges.dev>
2025-01-21 23:57:24 -0600

    fix bug with garbage collection
    - Dynamically created Command classes were being garbage collected by Python.

--- src/wonderland/app.py
+++ src/wonderland/app.py
@@ -5,11 +5,15 @@ from src.wonderland.pubsub import events
 
 class App:
     def __init__(self):
+        self.command_classes = []
         self.build_commands()
         self.command_registry = CommandRegistry()
         self.command_registry.load_commands()
 
     def build_commands(self):
-        CommandFactory.create_command(trigger="help", event_class=events.HelpInputEvent)
-        CommandFactory.create_command(trigger="create", event_class=events.CreateItemInputEvent, pos_args=["item_name"])
-        CommandFactory.create_command(trigger="look", event_class=events.LookInputEvent)
+        self.command_classes.extend([
+            CommandFactory.create_command(trigger="help", event_class=events.HelpInputEvent),
+            CommandFactory.create_command(trigger="create", event_class=events.CreateItemInputEvent, pos_args=["item_name"]),
+            CommandFactory.create_command(trigger="delete", event_class=events.DeleteItemInputEvent, pos_args=["item_name"]),
+            CommandFactory.create_command(trigger="look", event_class=events.LookInputEvent),
+        ])
